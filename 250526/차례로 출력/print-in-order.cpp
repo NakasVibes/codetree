63928bbedfdbf76a275190bcaf5063f7448c83a3
NakasVibes <ramoh@naver.com>
2025-05-26 17:18:30 +0900

    Title: 차례로 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250526/차례로 출력/print-in-order.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+    for (int a = 5; a < 18; a++) {
+        cout << a << " ";
+    }
+    return 0;
+}
