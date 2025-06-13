bab9d8326620dfcc7185559f21358e64c43ebed1
NakasVibes <ramoh@naver.com>
2025-06-13 16:24:42 +0900

    Title: 짝수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250613/짝수만 출력 2/output-only-even-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int b, a;
+    cin >> b >> a;
+
+    while ( b >= a ) {
+        if (b % 2 == 0) {
+            cout << b << " ";
+        }
+        b--;
+    }
+    return 0;
+}
