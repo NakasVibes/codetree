70253dd8bad6639250b94a676658e3114f6bf995
NakasVibes <ramoh@naver.com>
2025-06-13 10:02:17 +0900

    Title: 증가시키며 출력하기 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250613/증가시키며 출력하기 3/increase-and-print-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i = 10;
+    cin >> i;
+    
+    while (i <= 26) {
+        cout << i << " ";
+        i += 2;    
+    }
+    return 0;
+}
